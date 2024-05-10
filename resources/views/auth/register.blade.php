4a794fef80a6fa71b00fbf2c4bc9444ed5048830
juliv5 <jvelandia@umariana.edu.co>
2024-05-10 13:22:29 -0500

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -87,11 +87,22 @@
                                             <div class="text-center pt-1 mb-5 pb-1">
                                                 <button type="submit" class="btn btn-outline-danger">Registrarse</button>
                                             </div>
+
+                                            <div class="d-flex align-items-center justify-content-center pb-4">
+
+                                                <p class="mb-0 me-2">Ya tienes una cuenta?</p>
+                                                <br> </br>
+                                                <a href="{{ route('login') }}" class="btn btn-outline-danger">Iniciar Sesion</a>
+    
+                                            </div>
                                         </form>
 
                                     </div>
                                 </div>
+                                
                                 <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
+
+
                                     <div class="text-white px-3 py-4 p-md-5 mx-md-4">
 
                                         <br> </br><br> </br>
