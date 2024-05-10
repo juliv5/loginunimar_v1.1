2bdebb6b756b05e7ffc1d7b6f89af76adfd5bd20
juliv5 <jvelandia@umariana.edu.co>
2024-05-10 03:20:56 -0500

    to fix login error

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,107 +1,77 @@
-<!doctype html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <title>Title</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Register') }}</div>
 
-    <!-- Bootstrap CSS v5.2.1 -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+                <div class="card-body">
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
 
-    <link rel="stylesheet" href="{{asset('assets/estilos.css')}}">
-</head>
+                        <div class="row mb-3">
+                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
 
-<body>
-    <section class="h-100 gradient-form" style="background-color: #eee;">
-        <div class="container py-5 h-100">
-            <div class="row d-flex justify-content-center align-items-center h-100">
-                <div class="col-xl-10">
-                    <div class="card rounded-3 text-black">
-                        <div class="row g-0">
-                            <div class="col-lg-6">
-                                <div class="card-body p-md-5 mx-md-4">
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
-                                    <div class="text-center">
-                                        <img src="{{asset('assets/logoU.png')}}" style="width: 300px;" alt="logo">
-                                        <br> </br>
-                                        <h4 class="mt-1 mb-5 pb-1">Método de calificación Ingeniería de Sistemas Unimar.</h4>
-                                    </div>
-
-                                    <form action="{{route('login')}}" method="post">
-                                        @csrf
-                                        <center><b>INGRESA TUS DATOS</b></center>
-                                        <br> </br>
-
-                                        <div class="form-outline mb-4">
-                                            <input type="text" name="name" id="form2Example11" class="form-control" placeholder="Ingresar nombre" />
-                                            <label class="form-label" for="Juliana Velandia">Nombre</label>
-                                        </div>
-                                        <div class="form-outline mb-4">
-                                            <input type="email" name="email" id="form2Example11" class="form-control" placeholder="Ingresar correo electronico" />
-                                            <label class="form-label" for="ejemplo@umariana.edu.co">Correo Electronico</label>
-                                        </div>
-
-                                        <div class="form-outline mb-4">
-                                            <input type="password" name="password" id="form2Example22" class="form-control" placeholder="Ingresar contraseña" />
-                                            <label class="form-label" for="form2Example22">Contraseña</label>
-                                        </div>
-
-                                        <div class="form-outline mb-4">
-                                            <input type="password" name="password_confirmation" id="form2Example22" class="form-control" placeholder="Ingresa nuevamente tu contraseña" />
-                                            <label class="form-label" for="form2Example22">Confirmar contraseña</label>
-                                        </div>
-
-                                        <div class="text-center pt-1 mb-5 pb-1">
-                                            <a href="{{route('home')}}" class="btn btn-outline-danger">Registrarse</a>
-                                        </div>
-
-                                        <div class="d-flex align-items-center justify-content-center pb-4">
-                                            <p class="mb-0 me-2">Volver al inicio de sesión</p>
-                                            <br> </br>
-                                            <a href="{{route('login')}}" class="btn btn-outline-danger">Inicio de Sesión</a>
-
-                                        </div>
-
-                                    </form>
-
-                                </div>
-                            </div>
-                            <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
-                                <div class="text-white px-3 py-4 p-md-5 mx-md-4">
-
-                                    <br> </br><br> </br>
-                                    <h3 class="mb-7">Presentación del programa</h3>
-                                    <br> </br>
-
-                                    <p class="small mb-0">El Programa de Ingeniería de Sistemas de la Universidad Mariana procura la formación
-                                        integral del Ingeniero de Sistemas tanto en sus aspectos humanísticos como científicos que lo posibilita
-                                        a participar de manera activa y competente en la construcción de soluciones
-                                        informáticas encaminadas a satisfacer necesidades tecnológicas computacionales y
-                                        organizacionales del entorno local, regional, nacional e internacional, siempre actuante bajo los
-                                        principios y valores cristianos.</p>
-
-                                    <br> </br> <br> </br>
-                                    <img src="{{asset('assets/ingsis.jpg')}}" style="width: 300px;" alt="logo">
-
-                                </div>
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
                         </div>
-                    </div>
+
+                        <div class="row mb-3">
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            </div>
+                        </div>
+
+                        <div class="row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Register') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
-    </section>
-
-
-
-
-
-    <!-- Bootstrap JavaScript Libraries -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
-</body>
-
-</html>
+    </div>
+</div>
+@endsection
